65a1033f78e96d2d2f0bf36617783b95c8c6bceb
baibao577 <knotz22@gmail.com>
2023-02-28 13:20:33 +0700

    Feature 2 try SQUASH merge (#4)
    * just adding more functions
    
    * add "Add" function
    
    * add "Mul" function
    
    * Make README readable
    
    * readme readme

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ function plus(a, b) {
 function minus(a, b) {
   return a - b;
 }
+
+function minus(a, b) {
+  return a - b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
